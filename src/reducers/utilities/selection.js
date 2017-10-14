a9ae168224b080d2f8c817bfd944eeb83f56fab0
emmaoberstein <emmaoberstein@gmail.com>
2017-10-14 15:59:30 -0400

    groups functionality

--- src/reducers/utilities/selection.js
+++ src/reducers/utilities/selection.js
@@ -1,4 +1,5 @@
 import guid from 'guid';
+import { calculateBoundingBox } from './groups';
 
 export function selectShape(selected, shapeId, selectMultiple, shiftSelected) {
     if (!shapeId) { return selected; }
@@ -26,17 +27,46 @@ export function generateSelectionBoxes(selected, shapes) {
     const selectionBoxes = {};
     selected.map((id) => {
         const shape = shapes.byId[id];
-        selectionBoxes[id] = {
-            id: guid.create().toString(),
-            shapeId: id,
-            type: 'selectionBox',
-            x: shape.x,
-            y: shape.y,
-            width: shape.width,
-            height: shape.height
-        };
+        if (shape.type === "group") {
+            selectionBoxes[id] = generateGroupSelectionBox(id, shape, shapes);
+        } else {
+            selectionBoxes[id] = generateShapeSelectionBox(id, shape);
+        }
     });
     return selectionBoxes;
+}
+
+function generateShapeSelectionBox(id, shape) {
+    return {
+        id: guid.create().toString(),
+        shapeId: id,
+        type: 'selectionBox',
+        x: shape.x,
+        y: shape.y,
+        width: shape.width,
+        height: shape.height
+    };
+}
+
+function generateGroupSelectionBox(id, shape, shapes) {
+    let group = {
+        x: Infinity,
+        x2: 0,
+        y: Infinity,
+        y2: 0
+    };
+
+    group = calculateBoundingBox(shape, shapes, group);
+
+    return {
+        id: guid.create().toString(),
+        shapeId: id,
+        type: 'selectionBox',
+        x: group.x,
+        y: group.y,
+        width: group.x2 - group.x,
+        height: group.y2 - group.y
+    };
 }
 
 export function updateSelectionBoxes(shapes, selectionBoxes) {
