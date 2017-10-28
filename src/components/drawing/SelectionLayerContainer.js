1d74523eb76923553de09a5e7da02e3f8d822df5
collinmmckinney <collinmmckinney@gmail.com>
2017-10-28 11:26:49 -0400

    Missing files

---
+++ src/components/drawing/SelectionLayerContainer.js
@@ -0,0 +1,36 @@
+import { connect } from 'react-redux';
+import SelectionLayer from './SelectionLayer';
+import { handleDragStart, handleDrag, handleDragStop } from './../../actions/canvas';
+
+const mapStateToProps = ({ drawingState, menuState }) => {
+    const { selectionBoxes } = drawingState;
+    const { toolType } = menuState;
+    const selectionBoxesArray = Object.keys(selectionBoxes).map((id) => {
+        return selectionBoxes[id];
+    });
+
+    return {
+        selectionBoxes: selectionBoxesArray,
+        canvasTransformationMatrix: drawingState.canvasTransformationMatrix,
+        propagateEvents: toolType === 'rectangleTool' || toolType === 'lineTool' || toolType === 'panTool'
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onHandleDragStart: (shapeId, handleIndex, draggableData) => {
+            dispatch(handleDragStart(shapeId, handleIndex, draggableData));
+        },
+        onHandleDrag: (shapeId, handleIndex, draggableData) => {
+            dispatch(handleDrag(shapeId, handleIndex, draggableData));
+        },
+        onHandleDragStop: (shapeId, handleIndex, draggableData) => {
+            dispatch(handleDragStop(shapeId, handleIndex, draggableData));
+        }
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(SelectionLayer);
