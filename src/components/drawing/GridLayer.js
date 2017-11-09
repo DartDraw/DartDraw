dded8d3c7310638283fe9c1331b15240047d6b7c
Elisabeth Pillsbury <elisabeth.pillsbury@gmail.com>
2017-11-09 17:48:58 -0500

    non scaling stroke

--- src/components/drawing/GridLayer.js
+++ src/components/drawing/GridLayer.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 class GridLayer extends Component {
     static propTypes = {
+        scale: PropTypes.number,
         width: PropTypes.number,
         height: PropTypes.number,
         majorGrid: PropTypes.number,
@@ -10,16 +11,16 @@ class GridLayer extends Component {
     };
 
     render() {
-        const { width, height, majorGrid, minorGrid } = this.props;
+        const { scale, width, height, majorGrid, minorGrid } = this.props;
         return (
             <g>
                 <defs>
                     <pattern id="smallGrid" width={minorGrid} height={minorGrid} patternUnits="userSpaceOnUse">
-                        <path d={"M " + minorGrid + " 0 L 0 0 0 " + minorGrid} fill="none" stroke="#adadad" strokeWidth="0.5" vectorEffect="non-scaling-stroke" />
+                        <path d={"M " + minorGrid + " 0 L 0 0 0 " + minorGrid} fill="none" stroke="#adadad" strokeWidth={0.5 / scale} />
                     </pattern>
                     <pattern id="grid" width={majorGrid} height={majorGrid} patternUnits="userSpaceOnUse">
                         <rect width={majorGrid} height={majorGrid} fill="url(#smallGrid)" />
-                        <path d={"M " + majorGrid + " 0 L 0 0 0 " + majorGrid} fill="none" stroke="#adadad" strokeWidth="1" vectorEffect="non-scaling-stroke" />
+                        <path d={"M " + majorGrid + " 0 L 0 0 0 " + majorGrid} fill="none" stroke="#adadad" strokeWidth={1 / scale} />
                     </pattern>
                 </defs>
                 <rect width={width} height={height} fill="url(#grid)" pointerEvents="none" />
--- src/components/drawing/GridLayerContainer.js
+++ src/components/drawing/GridLayerContainer.js
@@ -2,9 +2,10 @@ import { connect } from 'react-redux';
 import GridLayer from './GridLayer';
 
 const mapStateToProps = ({ drawingState, menuState }) => {
-    const { canvasWidth, canvasHeight } = drawingState;
+    const { canvasWidth, canvasHeight, scale } = drawingState;
     const { majorGrid, minorGrid } = menuState;
     return {
+        scale: scale,
         width: canvasWidth,
         height: canvasHeight,
         majorGrid: majorGrid,
