9d420b40896fef4b9f22e06a265afd745d093b07
Luisa Vasquez <luna.v97@gmail.com>
2017-11-09 12:59:05 -0500

    updated text icon (#51)

--- src/components/left-menu/LeftMenu.js
+++ src/components/left-menu/LeftMenu.js
@@ -52,7 +52,7 @@ class LeftMenu extends Component {
                         <img src="./assets/marquee-zoom.svg" alt="marquee zoom" id="button-icon" />
                     </button>
                     <button onClick={() => this.handleToolSelect("textTool")}>
-                        <img src="./assets/frame.svg" alt="rect" id="button-icon" />
+                        <img src="./assets/text.svg" alt="rect" id="button-icon" />
                     </button>
                 </div>
             </div>
