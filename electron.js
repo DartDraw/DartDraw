fa9369e61ad00f72697dd73fd4c45c62c92f86a9
Trevor Davis <Trevor@Trevors-MacBook-Pro-4.local>
2018-01-15 16:09:20 -0500

    cleanup of electron.js

--- electron.js
+++ electron.js
@@ -203,6 +203,7 @@ const canvasItem = new MenuItem(
             {label: 'Resize Canvas'}
         ]}
 );
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
