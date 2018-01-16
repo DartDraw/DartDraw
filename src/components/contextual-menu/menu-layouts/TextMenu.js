180bdf50ae8c2bf40aa8e66a52ab55dd5a786878
Michelle Yu <michelle.p.yu.19@dartmouth.edu>
2018-01-15 23:30:21 -0500

    Updated Contextual Menu for Text

--- src/components/contextual-menu/menu-layouts/TextMenu.js
+++ src/components/contextual-menu/menu-layouts/TextMenu.js
@@ -13,7 +13,15 @@ class TextMenu extends Component {
 
         this.handleFontFamilyChange = this.handleFontFamilyChange.bind(this);
         this.handleFontSizeChange = this.handleFontSizeChange.bind(this);
+        this.handleFontStyleChange = this.handleFontStyleChange.bind(this);
+        this.handleFontWeightChange = this.handleFontWeightChange.bind(this);
         this.handleTextAlignChange = this.handleTextAlignChange.bind(this);
+        this.handleTextDecorationChange = this.handleTextDecorationChange.bind(this);
+        this.handleWidthChange = this.handleWidthChange.bind(this);
+        this.handleHeightChange = this.handleHeightChange.bind(this);
+        this.handleXChange = this.handleXChange.bind(this);
+        this.handleXChange = this.handleYChange.bind(this);
+
     }
 
     handleFontFamilyChange(event) {
@@ -28,10 +36,52 @@ class TextMenu extends Component {
         onEdit && onEdit(newText);
     }
 
+    handleFontStyleChange(event){
+        const { text, onEdit } = this.props;
+        const newText = Object.assign({}, text, { fontStyle: event.target.value });
+        onEdit && onEdit(newText);
+    }
+
     handleTextAlignChange(event){
         const { text, onEdit } = this.props;
         const newText = Object.assign({}, text, { textAlign: event.target.value});
         onEdit && onEdit(newText);
+    }
+
+    handleTextDecorationChange(event){
+        const { text, onEdit } = this.props;
+        const newText = Object.assign({}, text, { textDecoration: event.target.value});
+        onEdit && onEdit(newText);
+    }
+
+    handleFontWeightChange(event){
+        const { text, onEdit } = this.props;
+        const newText = Object.assign({}, text, { fontWeight: event.target.value });
+        onEdit && onEdit(newText);
+    }
+
+    handleWidthChange(event) {
+        const { rectangle, onEdit } = this.props;
+        const newRectangle = Object.assign({}, rectangle, { width: event.target.value });
+        onEdit && onEdit(newRectangle);
+    }
+
+    handleHeightChange(event) {
+        const { rectangle, onEdit } = this.props;
+        const newRectangle = Object.assign({}, rectangle, { height: event.target.value });
+        onEdit && onEdit(newRectangle);
+    }
+
+    handleXChange(event) {
+        const { rectangle, onEdit } = this.props;
+        const newRectangle = Object.assign({}, rectangle, { x: event.target.value });
+        onEdit && onEdit(newRectangle);
+    }
+
+    handleYChange(event) {
+        const { rectangle, onEdit } = this.props;
+        const newRectangle = Object.assign({}, rectangle, { y: event.target.value });
+        onEdit && onEdit(newRectangle);
     }
 
     render() {
@@ -56,11 +106,32 @@ class TextMenu extends Component {
                     <option value="72px">72</option>
                     <option value="100px">100</option>
                 </select>
+                <select value={this.props.text.fontStyle} onChange={this.handleFontStyleChange}>
+                    <option value="normal">normal</option>
+                    <option value="italic">italic</option>
+                    <option value="oblique">oblique</option>
+                </select>
+                <select value={this.props.text.fontWeight} onChange={this.handleFontWeightChange}>
+                    <option value="lighter">lighter</option>
+                    <option value="normal">normal</option>
+                    <option value="bold">bold</option>
+                </select>
                 <select value={this.props.text.textAlign} onChange={this.handleTextAlignChange}>
                     <option value="left">Left</option>
                     <option value="right">Right</option>
                     <option value="center">Center</option>
                 </select>
+                <select value={this.props.text.textDecoration} onChange={this.handleTextDecorationChange}>
+                    <option value="none">None</option>
+                    <option value="underline">Underline</option>
+                    <option value="line-through">Strikethrough</option>
+                </select>
+                <input value={this.props.text.x} />
+                <input value={this.props.text.y} />
+                <input value={this.props.text.width} />
+                <input value={this.props.text.height} />
+
+
             </div>
         );
     }
