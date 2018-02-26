cc4c71dc69a5a091fb9a2ba1cdca273ab141dd4e
Collin McKinney <collinmmckinney@gmail.com>
2018-02-26 12:31:14 -0500

    Ability to draw rounded rectangle on top of shapes

--- src/App.js
+++ src/App.js
@@ -68,7 +68,6 @@ class App extends Component {
                 <BottomScrollContainer />
                 <RightScrollContainer />
                 <ContextualMenuContainer />
-
             </div>
         );
     }
