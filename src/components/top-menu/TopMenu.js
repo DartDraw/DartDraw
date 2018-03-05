5b7594bfbd4dd1f671d3b781033f715d1fb053c2
Luisa <luisavasquez@Luisas-MacBook-Pro.local>
2018-03-05 14:09:53 -0500

    added grid snap icon

--- src/components/top-menu/TopMenu.js
+++ src/components/top-menu/TopMenu.js
@@ -158,12 +158,14 @@ class TopMenu extends Component {
                 <button onClick={this.handleFlipVertical}>
                     <img src="./assets/flip-vert.svg" alt="frontall" id="button-icon" />
                 </button>
-                <button onClick={this.handleToggleGridSnapping} id="button-icon">G</button>
+                <button onClick={this.handleToggleGridSnapping} id="button-icon">
+                    <img src="./assets/gridsnap.svg" alt="grid snap" id="button-icon" />
+                </button>
                 <button onClick={this.handleAlignmentClick}>
                     <img src="./assets/center-alignment.svg" alt="center-alignment" id="alignment-vertical" />
                 </button>
                 <button onClick={this.handleAlignmentClick}>
-                    <img src="./assets/vertical-alignment.svg" alt="center-alignment" id="alignment-horizontal" />
+                    <img src="./assets/vertical-alignment.svg" alt="vertical-alignment" id="alignment-horizontal" />
                 </button>
                 <button onClick={this.handleAlignmentClick}>
                     <img src="./assets/left-alignment.svg" alt="left-alignment" id="alignment-left" />
