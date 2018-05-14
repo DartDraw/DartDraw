8efcc0964f38e6ad9d25e4975d6cd64aaa17d30f
Luisa <luisavasquez@Luisas-MacBook-Pro.local>
2018-05-14 11:23:24 -0400

    added tooltips to left and top menu

--- src/components/left-menu/LeftMenu.js
+++ src/components/left-menu/LeftMenu.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './left-menu.css';
+import ReactTooltip from 'react-tooltip';
 
 class LeftMenu extends Component {
     static propTypes = {
@@ -33,43 +34,44 @@ class LeftMenu extends Component {
     render() {
         return (
             <div id="left-bar">
+                <ReactTooltip effect="solid" className="tooltip-style" />
                 <div id="basic-buttons">
-                    <button onClick={() => this.handleToolSelect("selectTool")} className={this.isSelected("selectTool")}>
+                    <button onClick={() => this.handleToolSelect("selectTool")} data-tip="Select" className={this.isSelected("selectTool")}>
                         <img src="./assets/001-cursor.svg" alt="select" id="button-icon" />
                     </button>
-                    <button onClick={() => this.handleToolSelect("panTool")} className={this.isSelected("panTool")}>
+                    <button onClick={() => this.handleToolSelect("panTool")} data-tip="Pan" className={this.isSelected("panTool")}>
                         <img src="./assets/scrollarrows.svg" alt="pan" id="button-icon" />
                     </button>
-                    <button onClick={() => this.handleToolSelect("rotateTool")} className={this.isSelected("rotateTool")} >
+                    <button onClick={() => this.handleToolSelect("rotateTool")} data-tip="Rotate" className={this.isSelected("rotateTool")} >
                         <img src="./assets/rotate.svg" alt="rotate" id="button-icon" />
                     </button>
                     <span className="separator" />
-                    <button onClick={() => this.handleToolSelect("rectangleTool")} className={this.isSelected("rectangleTool")}>
+                    <button onClick={() => this.handleToolSelect("rectangleTool")} data-tip="Rectangle" className={this.isSelected("rectangleTool")}>
                         <img src="./assets/rectangle.svg" alt="rect" id="button-icon" />
                     </button>
-                    <button onClick={() => this.handleToolSelect("roundedRectangleTool")} className={this.isSelected("roundedRectangleTool")}>
+                    <button onClick={() => this.handleToolSelect("roundedRectangleTool")} data-tip="Rounded Rectangle" className={this.isSelected("roundedRectangleTool")}>
                         <img src="./assets/roundedRect.svg" alt="rect" id="button-icon" />
                     </button>
-                    <button onClick={() => this.handleToolSelect("ellipseTool")} className={this.isSelected("ellipseTool")}>
+                    <button onClick={() => this.handleToolSelect("ellipseTool")} data-tip="Ellipse" className={this.isSelected("ellipseTool")}>
                         <img src="./assets/ellipse.svg" alt="ellipse" id="button-icon" />
                     </button>
-                    <button onClick={() => this.handleToolSelect("polygonTool")} className={this.isSelected("polygonTool")}>
+                    <button onClick={() => this.handleToolSelect("polygonTool")} data-tip="Polygon" className={this.isSelected("polygonTool")}>
                         <img src="./assets/hexagon.svg" alt="polygon" id="button-icon" />
                     </button>
                     <span className="separator" />
-                    <button onClick={() => this.handleToolSelect("lineTool")} className={this.isSelected("lineTool")}>
+                    <button onClick={() => this.handleToolSelect("lineTool")} data-tip="Line" className={this.isSelected("lineTool")}>
                         <img src="./assets/drawline.svg" alt="line" id="button-icon" />
                     </button>
-                    <button onClick={() => this.handleToolSelect("textTool")} className={this.isSelected("textTool")}>
+                    <button onClick={() => this.handleToolSelect("textTool")} data-tip="Text" className={this.isSelected("textTool")}>
                         <img src="./assets/text.svg" alt="rect" id="button-icon" />
                     </button>
-                    <button onClick={() => this.handleToolSelect("arcTool")} className={this.isSelected("arcTool")}>
+                    <button onClick={() => this.handleToolSelect("arcTool")} data-tip="Arc" className={this.isSelected("arcTool")}>
                         <img src="./assets/arc.svg" alt="arc" id="button-icon" />
                     </button>
-                    <button onClick={() => this.handleToolSelect("freehandPathTool")} className={this.isSelected("freehandPathTool")} >
+                    <button onClick={() => this.handleToolSelect("freehandPathTool")} data-tip="Freehand Path" className={this.isSelected("freehandPathTool")} >
                         <img src="./assets/freehand.svg" alt="freehand" id="button-icon" />
                     </button>
-                    <button onClick={() => this.handleToolSelect("bezierTool")} className={this.isSelected("bezierTool")}>
+                    <button onClick={() => this.handleToolSelect("bezierTool")} data-tip="Bezier Curve" className={this.isSelected("bezierTool")}>
                         <img src="./assets/bezier.svg" alt="bez" id="button-icon" />
                     </button>
                     <button onClick={this.handleExportSelect}>
